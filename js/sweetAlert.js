bf1de3f170a2bc74a60f60cd49fb7ceff10a7d94
Neo Thomas Titebe <94100024+UmbrellaSkiies@users.noreply.github.com>
2023-04-08 00:19:54 -0700

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,9 @@
+///////////////////////////////////////////////////////////////////////
+window.onload=function() {
+    swal({
+    title: "THANK YOU<br><small>for your support</small>!",
+    text: "Be with my upcoming project a real chatting box in php fully functioning.",
+    type: "success",
+    html: true
+    });
+}
